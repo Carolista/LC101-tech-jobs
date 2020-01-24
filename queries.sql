762bc47af96a3dd1730b3086d49d7e95d49cb607
Carolista <55961845+Carolista@users.noreply.github.com>
2020-01-24 16:39:54 -0600

    Parts 1 and 2 done

--- queries.sql
+++ queries.sql
@@ -1,7 +1,14 @@
 ## Part 1: Test it with SQL
 
+-- SELECT * from job;
+
 ## Part 2: Test it with SQL
 
+SELECT name from employer
+WHERE location = "St. Louis, MO";
+
 ## Part 3: Test it with SQL
+
+
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message="This field is required.")
+    @Size(min = 2, max = 60, message = "Please enter between 2-60 characters.")
+    private String location;
+
+    public Employer() {}
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
