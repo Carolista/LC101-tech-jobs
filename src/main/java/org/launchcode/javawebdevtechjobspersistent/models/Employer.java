2dc6a23281c12986f4353f9b49ee784f216046d4
Carolista <55961845+Carolista@users.noreply.github.com>
2020-01-26 14:49:59 -0600

    Part 3 done but not working... due to jobRepository not being autowired and included? going to branch off before attempting it myself

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,9 +15,14 @@ public class Employer extends AbstractEntity {
     @Size(min = 2, max = 60, message = "Please enter between 2-60 characters.")
     private String location;
 
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs = new ArrayList<>();
+
     public Employer() {}
 
     public Employer(String location) {
+        super();
         this.location = location;
     }
 
